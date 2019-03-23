18db05b21f327e1759c4007a86791d5a12e0c468
scary <imbamrgrey@gmail.com>
2019-03-23 21:31:39 +0100

    skills part

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import "./scss/main.scss";
+
 import "./components/header/header.scss"
 import "./components/profile/profile.scss"
 import "./components/header/header.pug"
 import "./components/contact/contact.pug"
-import "./components/contact/contact.scss"
 import "./components/skills/skills.scss"
 import "./index.pug";
+import "./components/main.scss"
 require('./fonts.scss');
 
 console.log(`I'm a silly entry point`);
